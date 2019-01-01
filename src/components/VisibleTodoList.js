893528ee87258439da232ab69715ea6b1e56f25d
den4ik <monastyrskiiden@gmail.com>
2019-01-01 17:44:46 +0200

    Fetching method added

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,22 +1,48 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { toggleTodo } from '../actions';
 import { getVisibleTodos } from '../reducers/index';
+import { fetchTodos } from '../api/index';
 import TodoList from './TodoList';
 
-const mapStateToProps = (state, { match }) => ({
-  todos: getVisibleTodos(state, match.params.filter || 'all')
-});
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos => {
+      console.log(this.props.filter, todos);
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos => {
+        console.log(todos);
+      });
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
+
+const mapStateToProps = (state, { match }) => {
+  const filter = match.params.filter || 'all';
+  return {
+    filter,
+    todos: getVisibleTodos(state, filter)
+  };
+};
 
 const mapDispatchToProps = {
   onTodoClick: toggleTodo
 };
 
-const VisibleTodoList = withRouter(
+VisibleTodoList = withRouter(
   connect(
     mapStateToProps,
     mapDispatchToProps
-  )(TodoList)
+  )(VisibleTodoList)
 );
 
 export default VisibleTodoList;
